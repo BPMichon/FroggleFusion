580e7313a05241b9144e34316a1351991693146b
cday6013 <cday6013@gmail.com>
2024-06-15 18:15:39 +0100

    tween animation broken

--- scenes/Frog1.gd
+++ scenes/Frog1.gd
@@ -11,14 +11,40 @@ func _ready():
 	if get_parent().has_meta("frog_count"):
 		get_parent().frog_count += 1
 	
-	
+	frogRandomMove()
 	pass # Replace with function body.
+	
+func frogRandomMove():
+	randomize()
+	await get_tree().create_timer(randf_range(0.5,2)).timeout
+
+	var x_position = randi_range(-10, 4)
+	var y_position = randi_range(-7, 4)
+	#position = Vector2(x_position, y_position) # greyed out because I don't want a jump to the new position
+
+
+	var tween
+	
+	tween.interpolate_property(self, "position",
+	position, Vector2(x_position, y_position), 1,
+	Tween.TRANS_EXPO, Tween.EASE_OUT)
+
+	tween.start()
+	await tween.finished
+	queue_free()
+	#while(not in boundary)
+		#randomly select a direction
+		#add a set move amount to that direction
+		#check if its in the boundary
+	#move the sprite
+	
+	frogRandomMove()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	  # Calculate movement step for this frame
-	var move_step = direction * speed * delta
-	# Update the node's position
-	position.x += move_step
+	#var move_step = direction * speed * delta
+	## Update the node's position
+	#position.x += move_step
 	pass
