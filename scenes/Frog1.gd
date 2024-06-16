6d37215526213ffc193d04fc634d35baa035d900
BogdanMichon <bogdanmichon14@gmail.com>
2024-06-16 12:03:19 +0100

    New Frog Added

--- scenes/Frog1.gd
+++ scenes/Frog1.gd
@@ -5,19 +5,21 @@ extends Sprite2D
 
 var direction = 1
 var speed = 100
-
+var hopFrame4 = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if get_parent().has_meta("frog_count"):
 		get_parent().frog_count += 1
+	
 	
 	frogRandomMove()
 	pass # Replace with function body.
 	
 
 func frogRandomMove():
+	
 	randomize()
-	await get_tree().create_timer(randf_range(0.5,2)).timeout
+	await get_tree().create_timer(randf_range(2,4)).timeout
 	
 	var isFrogOnScreen = false
 	var newPosition
@@ -42,11 +44,28 @@ func frogRandomMove():
 	#position = Vector2(x_position, y_position) # greyed out because I don't want a jump to the new position
 
 
+	
+	
+	%AnimatedSprite2D.play("hop")
+	while hopFrame4 != true:
+		print("NotHopping")
+	print("Hopping")
+	hopFrame4 = false
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "position", newPosition, 1)
-
-	#tween.start()
+	#Points Right 
+	#if flipped == false:
+		#if newPosition.x < position.x :
+			#flip frog (Point Left)
+	if %AnimatedSprite2D.flip_h == true:
+		if newPosition.x > position.x:
+			%AnimatedSprite2D.flip_h = false
+	else:
+		if newPosition.x < position.x:
+			%AnimatedSprite2D.flip_h = true
+	
 	await tween.finished
+	%AnimatedSprite2D.play("Idle")
 	#while(not in boundary)
 		#randomly select a direction
 		#add a set move amount to that direction
@@ -57,9 +76,15 @@ func frogRandomMove():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	#print(%AnimatedSprite2D.get_frame())
 	  # Calculate movement step for this frame
 	#var move_step = direction * speed * delta
 	## Update the node's position
 	#position.x += move_step
 	pass
+
+
+func _on_animated_sprite_2d_frame_changed():
+	print()
+	if %AnimatedSprite2D.get_frame() == 4:
+		hopFrame4 = true
